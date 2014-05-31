4fc7bb97195fb906dff698764c7b5390b61f45b3
Etienne van Delden <evdelden@i-level.nl>
2014-05-31 15:17:27 +0200

    Started code for making and editing quotes
    - added show pages for showing quotes
    - added form for creating / updating quotes

--- app/controllers/quote_controller.rb
+++ app/controllers/quote_controller.rb
@@ -8,4 +8,21 @@ def index
     @quotes = Quote.all
 end
 
+def show
+    @quote = Quote.find_by( params[ :id ] )
+end
+
+def new
+    @quote = Quote.new
+end
+
+def create
+    quote = Quote.create( quote_params )
+end
+
+private
+def quote_params
+    params.require( :quote ).permit( :quote, :author, :favorite )
+end
+
 end
