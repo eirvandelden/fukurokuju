3e65244c63abc194305d6300cf8987bbab41742a
Etienne van Delden <evdelden@i-level.nl>
2014-05-31 14:23:03 +0200

    Added animation effects to loading quotes
    - Added quote.js file. Gave block quote a slow fade animation and cite a faster animation when the quote has loaded

---
+++ app/assets/javascripts/quote.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+                  $(".quote > blockquote > p").fadeIn(3000, function(){
+                                                      $(".quote > blockquote > cite").hide().fadeIn(500);
+                                                      });
+
+                  });
