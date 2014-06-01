6e4de79514fbccafc33b764a1bc1e829de1948d4
Etienne van Delden <evdelden@i-level.nl>
2014-06-01 21:22:35 +0200

    Finished rename

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530145303) do
+ActiveRecord::Schema.define(version: 20140601112542) do
 
   create_table "quotes", force: true do |t|
     t.text     "quote"
